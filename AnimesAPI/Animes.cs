0b731007c429985a6c4f78afca96a2ce73920904
EmmanuelEngelbert <emmanuel.engelbert@outlook.com>
2022-09-20 09:02:03 +0100

    Initial Project Structure

---
+++ AnimesAPI/Animes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AnimesAPI
+{
+    public class Animes
+    {
+        public int Id { get; set; }
+        public string AnimeTitle { get; set; } = string.Empty;
+        public int YearOfProduction { get; set; }
+        public string AnimeGenre { get; set; } = string.Empty;
+        public string AnimeStudio { get; set; } = string.Empty;
+
+
+
+    }
+}
+
